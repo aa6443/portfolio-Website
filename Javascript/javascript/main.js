1b86a3443b6a106940e73fb6079bc286200f35a3
aa6443 <aa6443@srmist.edu.in>
2023-10-21 12:29:25 +0530

    basic changes in code

---
+++ Javascript/javascript/main.js
@@ -0,0 +1,17 @@
+//strings 
+const myVariable = "Mathematics";
+
+//the length property
+console.log(myVariable.length);
+console.log("Hello my".length);
+
+//string methods 
+console.log(myVariable.charAt(5));
+
+console.log(myVariable.indexOf("mat")); 
+
+console.log(myVariable.slice(5));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.includes("mat"));
+console.log("hello world".split(" "));
