8167ec5d259d70638901a99a1761df426bf2ff1e
Aniket Kumar <aa6443@srmist.edu.in>
2023-10-27 08:19:43 +0530

    learning how to clone a repo

---
+++ Javascript/test.js
@@ -0,0 +1,14 @@
+//conversion and operations 
+
+let score = "33abc"
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score) //n is capital 
+console.log(typeof(valueInNumber))
+console.log((valueInNumber)) // NaN -> Not a Number 
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+ 
